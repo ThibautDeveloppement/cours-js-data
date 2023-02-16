96dc18adfc42981ecbcc522cbe2bd1eec98cb576
Thibaut Degombert <thibaut.dev.pro@gmail.com>
2023-02-16 04:48:45 -0600

    suite cours data

--- index.js
+++ index.js
@@ -55,4 +55,44 @@ else{
     // console.log(data[1].pseudo + " est plus vieille que " + data[0].pseudo);
 }
 
+
+//while
+
+let w = 0;
+
+while (w < 10){
+    w++;
+    console.log("la valeur de w est de : " + w)
+}
+
+//do while
+let d = 0;
+
+do {
+    d++
+    // console.log(d)
+} while (d < 5);
+
+// le while et le do while ne sont pas tres utiliser contrairement a se qui suit 
+
+
+//Les boucles for
+
+for (const user of data) {
+    //document.body.innerHTML += <li>${user.pseudo} - ${user.age} ans</li>;
+}
+
+// on declare la valeur de i / jusqu'ou on boucle / on incremente i si la comndition 2 nest pas remplie
+for (i = 0; i < data.length; i++) {
+    //console.log(i);
+    //console.log(data[i].technos[0]);
+    //document.body.innerHTML += "<h2>" + data[i].technos + "</h2>";
+}
+
+document.body.addEventListener("click", () => {
+    console.log(e.target.id);
+})
+
+
+
 //ce cours prepare la gestion de data de javascript
